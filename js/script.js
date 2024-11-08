a7d230c17cea5397255e2aa62f1194b53be37dc5
Enricima <mathis.enrici@gmail.com>
2024-11-08 19:39:40 +0100

    complete pages with style and anims

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/** TYPING ANIMATION */
+var typed = new Typed('.typing', {
+    strings: ["" ,"Sound Engineer", "Sound Mixer", "Sound Editor", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
